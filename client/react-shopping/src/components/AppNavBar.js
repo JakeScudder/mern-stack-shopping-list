2c86038450f5ad9b8ddafb0a11e85879d8f1fa1b
Jake Scudder <scudsterr@gmail.com>
2020-06-05 14:00:02 -0400

    React Built Out

---
+++ client/react-shopping/src/components/AppNavBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+
+class AppNavBar extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-b">
+          <Container>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/JakeScudder">
+                    Github
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavBar;
